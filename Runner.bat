09481c86a353b528f74d910300768e7538d78ef0
HarshaKB-TY <harsha.k@testyantra.com>
2022-10-11 10:14:49 +0530

    commit

--- Runner.bat
+++ Runner.bat
@@ -1,7 +1,5 @@
 %~d1
-
 cd "%~p1"
-
 java -cp "./lib/*;target/classes;target/test-classes" com.tyss.trello.testsuitegenerator.TestSuiteGenerator
 java -cp "./lib/*;target/classes;target/test-classes" org.testng.TestNG TestSuite.xml
 
